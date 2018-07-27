f6f6696a2432bf25e0b3f88409e7c5e6d3cc2360
Aleksandr <lync@live.ru>
2018-07-28 00:09:19 +0300

    Changed api to custom json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-let fakeServerData = [
-  {
-    name: "Bill",
-    age: 23
-  },
-  {
-    name: "John",
-    age: 25
-  }
-];
-
 class Person extends Component {
   render(){
     let data = this.props.data;
     return (
       <div>
         <div>
-          <b>{data.title}</b>
+          <b>{data.title}</b><br/>
+          {data.type}
         </div>
         <div>
           <img src={data.image_url} alt={data.title}/>
@@ -32,34 +22,43 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      serverData:[],
-      realServerData:{},
-      filterString: 'pizza'
+      serverData: "",
+      filterString: 'sushi roll'
     };
   }
 
   componentDidMount() {
-    this.setState({serverData: fakeServerData});
-
-    const key = "8699278b36f1587611b0adfbc56773b5";
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-
+    //const key = "8699278b36f1587611b0adfbc56773b5";
+    //const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const customData = "https://api.jsonbin.io/b/5b5b8601f24d8943d04eebf2/5"
+    
+    /*
     fetch(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.state.filterString}`)
     .then(response => response.json())
-    .then(data => this.setState({serverData: data.recipes.slice(0,8)}));
+    .then(data => {console.log(data); return this.setState({serverData: data.recipes.slice(1,9)})});
+    */
+
+    fetch(`${customData}`)
+    .then(response => response.json())
+    .then(data => {console.log(data); return this.setState({serverData: data.recipes})});
   }
 
   render() {
+    let serverData = this.state.serverData;
     return (
-
       <div className="App">
-        <header className="">
-        </header>
-        <main>
-          {this.state.serverData.map((x, key) => 
-            <Person data = {x} key = {key}/>
-          )}
-        </main>
+        {serverData !== "" ?
+        <div className="content">
+          <header className="">
+            <button onClick={() => this.setState({filterString: "sushi roll"})}>Sushi</button>
+            <button onClick={() => this.setState({filterString: "ramen"})}>Ramen</button>
+          </header>
+          <main>
+            {serverData.map((x, key) => 
+              <Person data = {x} key = {key}/>
+            )}
+          </main>
+        </div> : "Loading..."}
       </div>
     );
   }
