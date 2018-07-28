7686c88e2ca60ec8a22e5d241c2fffc8658e0391
Aleksandr <lync@live.ru>
2018-07-28 19:03:06 +0300

    Styling, api fixes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ class Person extends Component {
   render(){
     let data = this.props.data;
     return (
-      <div>
-        <div>
-          <b>{data.title}</b><br/>
-          {data.type}
+      <div className="food_container">
+        <div className="food_title">
+          {data.title}
         </div>
-        <div>
-          <img src={data.image_url} alt={data.title}/>
+        <div  className="food_info">
+          <div className="small_info">
+            {data.amount > 1 ? `${data.amount} pieces`: ""}
+          </div>
+          <div className="">
+            {data.contents.map(x => x)}<br/>
+            {`Price: ${data.price}`}
+          </div>
+        </div>
+        <div className="add_container">
+          <div  className="food_add">
+            ADD
+          </div>
         </div>
       </div>
     );
@@ -23,22 +33,25 @@ class App extends Component {
     super();
     this.state = {
       serverData: "",
-      filterString: "sushi",
-      filteredData: ""
+      filterString: "sushi"
     };
   }
 
+  
+
   componentDidMount() {
-    //const key = "8699278b36f1587611b0adfbc56773b5";
-    //const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const customData = "https://api.jsonbin.io/b/5b5b8601f24d8943d04eebf2/5"
-    
     /*
+    const key = "8699278b36f1587611b0adfbc56773b5";
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+    
     fetch(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.state.filterString}`)
     .then(response => response.json())
     .then(data => {console.log(data); return this.setState({serverData: data.recipes.slice(1,9)})});
     */
 
+    const customData = "https://api.jsonbin.io/b/5b5b8601f24d8943d04eebf2/14"
+    
     fetch(`${customData}`)
     .then(response => response.json())
     .then(data => this.setState({serverData: data.recipes}))
@@ -47,12 +60,14 @@ class App extends Component {
   componentDidUpdate() {
     
   }
+  
 
   render() {
     let serverData = this.state.serverData;
 
-    if(this.state.serverData !== "")
+    if(this.state.serverData !== ""){
       serverData = this.state.serverData.filter(x => x.type === this.state.filterString);
+    }
       
     return (
       <div className="App">
@@ -60,9 +75,9 @@ class App extends Component {
         <div className="content">
           <header className="">
             <button onClick={() => this.setState({filterString: "sushi"})}>Sushi</button>
-            <button onClick={() => this.setState({filterString: "sashimi"})}>sashimi</button>
-            <button onClick={() => this.setState({filterString: "nigiri"})}>Ramen</button>
-            <button onClick={() => this.setState({filterString: "miso soup"})}>miso soup</button>
+            <button onClick={() => this.setState({filterString: "nigiri"})}>Nigiri</button>
+            <button onClick={() => this.setState({filterString: "ramen"})}>Ramen</button>
+            <button onClick={() => this.setState({filterString: "miso soup"})}>Miso Soup</button>
           </header>
           <main>
             {serverData.map((x, key) => 
